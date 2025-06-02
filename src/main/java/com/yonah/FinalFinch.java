3b73650292bfb336b62c411c852690fea2b01695
yonahpeebles <ypeebles0001@mymail.lausd.net>
2025-06-02 15:25:52 -0700

    updated final project

--- src/main/java/com/yonah/FinalFinch.java
+++ src/main/java/com/yonah/FinalFinch.java
@@ -7,7 +7,17 @@ public class FinalFinch {
         Finch bird = new Finch();
 
         // What do I want my bird to do?
+        bird.setBeak(50, 100, 100);
+        bird.pause(1);
+        bird.setTurn ("R", 30, 50);
+        bird.setMove("F", 10, 75);
+        bird.setTurn ("R", 30, 50);
+        bird.setMove("F", 10, 75);
+        
+        
 
+
+        
 
 
         bird.stopAll();
