a52d99d632a981fdb3dde938bf3e77aaa9916597
yonahpeebles <ypeebles0001@mymail.lausd.net>
2025-06-02 15:01:05 -0700

    added FinalFInch.java

---
+++ src/main/java/com/yonah/FinalFinch.java
@@ -0,0 +1,16 @@
+package com.yonah;
+
+import com.birdbrain.Finch;
+
+public class FinalFinch {
+    public static void main(String[] args) {
+        Finch bird = new Finch();
+
+        // What do I want my bird to do?
+
+
+
+        bird.stopAll();
+        bird.disconnect();
+    }
+}
